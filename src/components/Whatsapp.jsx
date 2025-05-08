b65ecda726dc59512b513b45553231033f1c9f77
alexamber2021@gmail.com <alexamber2021@gmail.com>
2025-05-08 12:25:20 -0700

    whatsapp api changes

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -4,9 +4,8 @@ import WhatsappIcon from '../assets/images/whatsapp.png'
 function Whatsapp() {
   return (
     <div style={{zIndex: "6", left:"initial", bottom:"25px", position:"fixed", right:"15px"}}>
-       <a href="https://api.whatsapp.com/send?phone=6042172992&text=Hello%20Gavin,%20I%20am%20interested%20in%20your%20services" target="_blank" rel="noopener noreferrer">
-        
-        <img src={WhatsappIcon} alt="Whatsapp"/>
+        <a href="https://api.whatsapp.com/send?phone=+16042172992&text=Hello%20Gavin,%20I%20am%20interested%20in%20your%20services" target="_blank" rel="noopener noreferrer">
+              <img src={WhatsappIcon} alt="Whatsapp"/>
         </a>
         </div>
   )
